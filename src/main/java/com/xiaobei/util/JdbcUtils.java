176c566ae0ec5e82ef94702db7e4d5c5494fe011
ruxiaobei <ruhf@qeebike.com>
2018-11-01 22:03:48 +0800

    再次提交

--- src/main/java/com/xiaobei/util/JdbcUtils.java
+++ src/main/java/com/xiaobei/util/JdbcUtils.java
@@ -1,15 +1,14 @@
 package com.xiaobei.util;
 
+import org.apache.commons.dbcp.BasicDataSourceFactory;
+
+import javax.sql.DataSource;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
-
-import javax.sql.DataSource;
-
-import org.apache.commons.dbcp.BasicDataSourceFactory;
 /**
  * 基于mysql的jdbc工具类
  * @author Administrator
